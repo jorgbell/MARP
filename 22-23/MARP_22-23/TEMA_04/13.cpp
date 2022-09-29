fbc3e93e52591fef17ad595960939744c20d0d34
jorgbell <jorgbell@ucm.es>
2022-09-29 10:36:02 +0200

    ejercicio de evaluacion 13 wrong answer

--- 22-23/MARP_22-23/TEMA_04/13.cpp
+++ 22-23/MARP_22-23/TEMA_04/13.cpp
@@ -8,6 +8,8 @@
 #include <iostream>
 #include <fstream>
 #include <queue>
+#include <vector>
+#include <cmath>
 using namespace std;
 
 
@@ -24,8 +26,29 @@ using namespace std;
  // Escribe el código completo de tu solución aquí debajo
  // ================================================================
  //@ <answer>
+using mQueue = priority_queue<int, vector<int>, std::greater<int>>;
 
-bool resuelveCaso12() {
+
+void resolver13(priority_queue<int>&cola1, mQueue & cola2, int n) {
+    int a = cola1.top();
+    if (n > a)
+        cola2.push(n);
+    else
+        cola1.push(n);
+
+    int s1 = cola1.size(); int s2 = cola2.size();
+
+    if (s1-s2 > 1) {
+        int top = cola1.top(); cola1.pop(); cola2.push(top);
+    }
+    else if (s2-s1 > 1) {
+        int top = cola2.top(); cola2.pop(); cola1.push(top);
+    }
+
+}
+
+
+bool resuelveCaso13() {
 
     // leer los datos de la entrada
     int N, M;
@@ -33,22 +56,18 @@ bool resuelveCaso12() {
     if (N == 0 && M == 0)
         return false;
 
-    priority_queue<int, vector<int>, std::greater<int>> pajaros;
-    pajaros.push(N);
+    priority_queue<int> cola1;
+    cola1.push(N);
+    mQueue cola2;
     // resolver el caso posiblemente llamando a otras funciones
     for (int i = 0; i < M; i++) {
         int a, b;
         cin >> a >> b;
-        pajaros.push(a); pajaros.push(b);
-        priority_queue<int, vector<int>, std::greater<int>> alt(pajaros);
-        for (int i = 0; i < alt.size() / 2; i++) {
-            alt.pop();
-        }
-        cout << alt.top() << " ";
-
+        resolver13(cola1, cola2, a);
+        resolver13(cola1, cola2, b);
+        cout << cola2.top() << " ";
     }
     // escribir la solución
-    
 
     return true;
 }
@@ -59,11 +78,11 @@ bool resuelveCaso12() {
 int main() {
     // ajustes para que cin extraiga directamente de un fichero
 #ifndef DOMJUDGE
-    std::ifstream in("casos12.txt");
+    std::ifstream in("casos13.txt");
     auto cinbuf = std::cin.rdbuf(in.rdbuf());
 #endif
 
-    while (resuelveCaso12());
+    while (resuelveCaso13());
 
     // para dejar todo como estaba al principio
 #ifndef DOMJUDGE
